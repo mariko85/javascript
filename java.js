056b96ab06b469bd2c42e330cef607d6fb8a8e48
mariko85 <m.chaxvashvili@gmail.com>
2025-01-28 19:51:16 +0400

    first commit

---
+++ java.js
@@ -0,0 +1,35 @@
+let fruits = ["apple", "banana", "cherry"]; 
+fruits.push("orange" )
+console.log(fruits)
+
+
+let numbers = [10, 20, 30, 40];  
+numbers.shift()
+console.log(numbers)
+
+let colors = ["red", "blue", "green"]; 
+colors.splice(1,0,"yellow")
+console.log(colors)
+
+let animals1 = ["cat", "dog"]; 
+let animals2 = ["lion", "tiger"]; 
+let animals=animals1.concat(animals2)
+console.log(animals)
+
+
+let scores = [50, 30, 20, 10, 40]; 
+scores.sort()
+console.log(scores)
+
+let cities = ["Tbilisi", "Batumi", "Kutaisi"]; 
+cities.join("-")
+console.log(cities)
+
+let letters = ["a", "b", "c", "d"]; 
+letters.reverse()
+console.log(letters)
+
+let books = ["Math", "Science", "History"]; 
+
+console.log(books.length)
+
