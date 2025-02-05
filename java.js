3e57c4d7993b5441c685cc5a6d83694c654c901a
mariko85 <m.chaxvashvili@gmail.com>
2025-02-05 18:57:12 +0400

    loop

--- java.js
+++ java.js
@@ -1,341 +1,127 @@
 
-// // push -დამატება ბოლოში
-// let array =[ 1,5,6,'mari'];
-// array.push('nino');
-// console.log(array)
+ 
+//  //loop 
 
-// // unshift დამატება თავში
-// let array =[ 1,5,6,'mari'];
-// array.unshift(5);
-// console.log(array)
 
+//   რიცხვების დაბეჭდვა 1-დან 10-მდე:  for ციკლით: დაწერეთ კოდი, რომელიც for ციკლის გამოყენებით დაბეჭდავს რიცხვებს 1-დან 10-მდე. 
 
-// // pop წაშლა ბოლოში
-// let array =[ 1,5,6,'mari'];
-// array.pop();
-// console.log(array)
+// ( არ არის სავალდებულო)while ციკლით: დაწერეთ კოდი, რომელიც while ციკლის გამოყენებით დაბეჭდავს რიცხვებს 1-დან 10-მდე. 
 
-
-// // shift წაშლა თავში
-// let array =[ 1,5,6,'mari'];
-// array.shift();
-// console.log(array)
-
-// // splice წაშლა და დაამტება ინდექსით ამოჭრა 
-// let array =[ 1,5,6,'mari', 8 , 9 , 4 ];
-// array.splice( 2, 3, "mak")
-// console.log(resalt)
-
-
-// // //reverc amotrialeba
-//  let array =[ 1,5,6,'mari', 8 , 9 , 4 ];
-// array.reverse( )
-//  console.log(array)
-
-
-// // //sort amotrialeba
-//  let array =[ 1,5,6,'mari', 8 , 9 , 4 ];
-// array.sort( )
-//   console.log(array)
-
-
-
-
-// // //slice ამოჭრა მე-4 ინდექსიდან მე-4 მდე
-// let array =[ 1,5,6,'mari', 8 , 9 , 4 ];
-// let m=array.slice( 2,4)
-//   console.log(m)
-
-// // //concate შეერთბა
-//  let array1 =[ 1,5,6,'mari', 8 , 9 , 4 ];
-//  let array2 =[ 3,0];
-
-//  let array= array1.concat( array2)
-//    console.log(array)
-
-
-// // //includ შეicavs
-// let array1 =[ 1,5,6,'mari', 8 , 9 , 4 ];
-//  let array= array1.includes( 4)
-//    console.log(array)
-
-// // //join შეერთბა
-// let array1 =[ 1,5,6,'mari', 8 , 9 , 4 ];
-// let array2 =[ 3,0];
-//  let array= array1.join( "/  ")
-//    console.log(array)
-
-
-// // //სტრინგ მეთოდები at ამოღება ბოლოდან რომელიმე სიმბოლოსი
-// let text ="hello"
-// let resalt=text.at(-4)
-// console.log(resalt)
-
-
-// // //სტრინგ მეთოდები chartat ამოღება Tavidan რომელიმე სიმბოლოსი
-// let text ="hello"
-// let resalt=text.charAt(1)
-// console.log(resalt)
-
-// // // // concate
-// let text ="hello"
-//  let resalt=text.concat(' ', 'word')
-//  console.log(resalt)
-
-
-//  // // // startsWith იწყება თუ არა ჰ -ით
-//  let text ="hello"
-//  let resalt=text.startsWith('h')
-//  console.log(resalt)
-
-
-//  // // // endsWith იწყება თუ არა ჰ -ით
-//  let text ="hello"
-//  let resalt=text.endsWith('h')
-//  console.log(resalt)
-
-
-//  // // // include შეიცავს თუ არა ჰ -ით
-//  let text ="hello"
-//  let resalt=text.includes('l')
-//  console.log(resalt)
-
-
-// // // // indexOf dasawyisidan meramdenea o
-// let text ="hello"
-// let resalt=text.indexOf('o')
-// console.log(resalt)
-
-// // // // lastIndex bolodan meremdenea h -ით
-// let text ="hello"
-// let resalt=text.lastIndexOf('h')
-// console.log(resalt)
-
-
-// // // // reper gameoreba
-//  let text ="hello"
-// let resalt=text.repeat(2)
-// console.log(resalt)
-
-// // // // replace gameoreba
-// let text ="hello"
-// let resalt=text.replace('el', 'k')
-// console.log(resalt)
-
-// // // // slice მოჭრა
-// let text ="hello"
-// let resalt=text.slice(2,3)
-// console.log(resalt)
-
-// // // // split მასივად გადაქცევა
-// let text ="hello"
-// let resalt=text.split('')
-// console.log(resalt)
-
-
-// // // // substring მოჭრა
-// let text ="hello"
-// let resalt=text.substring(1,2)
-// console.log(resalt)
-
-
-// // // // toLowerCase გარდაქმნის პატარა ასოებად
-// let text ="HEllo"
-//  let resalt=text.toLowerCase()
-//  console.log(resalt)
-
-
-
-// // // //toUpperCase გარდაქმნის did ასოებად
-// let text ="hello"
-//  let resalt=text.toUpperCase()
-//  console.log(resalt)
-
-// // //toUpperCase გარდაქმნის did ასოებად
-// let text ='hello'
-//  let resalt=text.startsWith()
-//  console.log(resalt)
-// // let fruits = ["apple", "banana", "cherry"]; 
-// // fruits.push("orange" )
-// // console.log(fruits)
-
-
-// // let numbers = [10, 20, 30, 40];  
-// // numbers.shift()
-// // console.log(numbers)
-
-// // let colors = ["red", "blue", "green"]; 
-// // colors.splice(1,0,"yellow")
-// // console.log(colors)
-
-// // let animals1 = ["cat", "dog"]; 
-// // let animals2 = ["lion", "tiger"]; 
-// // let animals=animals1.concat(animals2)
-// // console.log(animals)
-
-
-// // let scores = [50, 30, 20, 10, 40]; 
-// // scores.sort()
-// // console.log(scores)
-
-// // let cities = ["Tbilisi", "Batumi", "Kutaisi"]; 
-// // cities.join("-")
-// // console.log(cities)
-
-// // let letters = ["a", "b", "c", "d"]; 
-// // letters.reverse()
-// // console.log(letters)
-
-// // let books = ["Math", "Science", "History"]; 
-
-// // console.log(books.length)
-
-// // დავალება 1: სტრიქონის სიგრძე
-// // შეამოწმეთ მოცემული სტრიქონის სიგრძე და დააბეჭდეთ, რამდენი სიმბოლოა მასში.
-// let text = 'School';
-// let result = text.length;
-// console.log(result);
-
-
-// // დავალება 2: სტრიქონის ყველა ასო დიდი
-// // შეიტანეთ სტრიქონი და გამოიტანეთ ის მხოლოდ დიდი ასოებით.
-// let text1 ="School";
-// let result1 = text1.toUpperCase();
-// console.log(result1);
-
-
-// // დავალება 3: სტრიქონის ყველა ასო პატარა
-// // შეიტანეთ სტრიქონი და გამოიტანეთ ის მხოლოდ პატარა ასოებით.
-
-// let text3= "School";
-// let result3=text3.toLowerCase();
-// console.log(result3);
-
-
-// // // დავალება 4: სტრიქონის კონკრეტული სიმბოლო
-// // // შეიტანეთ სტრიქონი და შეამოწმეთ, რა სიმბოლოა კონკრეტულ ინდექსზე.
-// // let text4 ="School";
-// // let result4 = text4.indexOf(h);
-// // console.log(result4);
-
-
-// // დავალება 5: სტრიქონის ნაწილი
-// // შეიტანეთ სტრიქონი და ამოჭერით მხოლოდ ნაწილობრივი სტრიქონი (ინდექსებით განსაზღვრული).
-
-// let text5 =" School";
-// let result5 = text5.slice(1,4);
-// console.log(result5);
-
-
-// // დავალება 6: სტრიქონის ცვლილება
-// // შეიტანეთ სტრიქონი და შეცვალეთ სიტყვა "hello" სიტყვით "hi".
-// let text6 =" hello";
-// let result6 = text6.replace("hello" , "hi")
-// console.log(result6);
-
-
-// // // 
-// // დავალება 7: სტრიქონის განაწილება
-// // შეიტანეთ სტრიქონი, რომელშიც სიტყვები გამოყოფილია მძიმეთი, და გაყავით სტრიქონი შესაბამის ნაწილებად (მიუთითეთ split() მეთოდზე).
-// let text7 ="hello Word";
-// let result7 = text7.split()
-// console.log(result7);
-
-
-
-// // დავალება 8: სტრიქონის დასაწყისი
-// // შეამოწმეთ, იწყება თუ არა სტრიქონი სიტყვით "Java"
-// let text8 ="hello";
-// let result8 = text8.startsWith("java")
-// console.log(result8);
-
-
-// // დავალება 9: სტრიქონის ბოლოს
-// // შეამოწმეთ, მთავრდება თუ არა სტრიქონი სიტყვით "Script".
-// let text9 ="hello";
-// let result9 = text9.endsWith("Script")
-// console.log(result9);
-
-
-// // დავალება 10: სტრიქონის გამეორება
-// // შეიტანეთ სტრიქონი და გამეორეთ ის 4-ჯერ.
-// let text10 ="hello";
-// let result10 = text10.repeat(4)
-// console.log(result10);
-
-// // დავალება 11: სტრიქონის ცარიელი სივრცეების მოცილება
-// // შეიტანეთ სტრიქონი, რომელშიც იქნება ცარიელი ადგილები დასაწყისსა და ბოლოში. წაშალეთ ეს ცარიელი ადგილები.
-// let text11 ="   hello";
-// let result11 = text11.trim()
-// console.log(result11);
-
-
-// // დავალება 12: სტრიქონის გამოყოფა 
-
-// // შეიტანეთ სტრიქონი და შეამოწმეთ, თუ შეიცავს თუ არა ის სიტყვას "JavaScript".
-
-// let text12 ="   hello";
-// let result12 = text12.includes('JavaScript')
-// console.log(result12);
-
-
-// let fullName = (prompt('დაწერე სახელი'))
-// let resalt = fullName.charAt(0).toUpperCase()+ fullName.slice(1).toLowerCase();
-
-// console.log(`ჩემი სახელია ${Text}`);
-
-
-// let fullName = (prompt('დაწერე სახელი'))
-//  console.log(`ჩემი სახელია ${fullName}`);
-
-
-
-//  შექმენით ობიექტი, რომელიც შეიცავს შემდეგ მონაცემებს: 
-
-// let student = {
-//     fullname: 'mari',
-//     age: 50,
-//     study:true,
-//     subject :  [ 'Math', 'geografi', 'biology'],
-//     addres:{
-//      city : 'tbilisi',
-//       street:'meunargia' },
-
+// for (let i=1; i<10 ; i++ ) {
+//    console.log(i);
 // }
 
-// console.log(student)
 
-
-// შექმენი ობიექტი  წიგნის  
-
-// let book = {
-//     aoutor : 'ilia',
-//     year: 1985 ,
-//     ilia : true,
-//     janr: {
-//         aoutor :'chavchavadze',
-//         aoutor :' wereteli',
-//     },
+// let i=1;
+// while(i<10) {
+//     console.log(i);
+//     i++;
 // }
-// console.log(book)
 
 
 
-// შექმენი ობიექტი, რომელიც აღწერს თეატრის ბილეთს. 
+// ლუწი რიცხვების დაბეჭდვა 2-დან 20-მდე: for ციკლით: დაწერეთ კოდი, რომელიც for ციკლის გამოყენებით დაბეჭდავს ლუწ რიცხვებს 2-დან 20-მდე. 
 
-let tiket = {
-    tiketNumber: 50,
-    price: 20 ,
-    Available : true,
-    addres: {
-        city :'tbilisi',
-        street:' Wavchavadze',
-    },
+// ( არ არის სავალდებულო)while ციკლით: დაწერეთ კოდი, რომელიც while ციკლის გამოყენებით დაბეჭდავს ლუწ რიცხვებს 2-დან 20-მდე. 
+
+
+// for (let i=2; i<20 ; i++) {
+//       console.log(i);
+//    }
+
+// //   რიცხვების ამოწერა უკუღმა:  for ციკლით: დაწერეთ კოდი, რომელიც for ციკლის გამოყენებით დაბეჭდავს რიცხვებს 10-დან 1-მდე.
+// //  ( არ არის სავალდებულო)while ციკლით: დაწერეთ კოდი, რომელიც while ციკლის გამოყენებით დაბეჭდავს რიცხვებს 10-დან 1-მდე. 
+
+
+// for (let i = 10; i > 0 ; i--){
+//     console.log(i);
+// }
+
+// let i = 10;
+// while (i> 0){
+//     console.log(i);
+//     i --;
+// }
+
+
+//   რიცხვის ლუწი ან კენტი შემოწმება: 
+
+// დაწერეთ კოდი, რომელიც იღებს რიცხვს და ამოწმებს, არის თუ არა ის ლუწი თუ კენტი. თუ რიცხვი ლუწია, დაბეჭდეთ "რიცხვი ლუწია", ხოლო თუ კენტია - "რიცხვი კენტია".
+
+
+
+//   რიცხვის დადებითი, უარყოფითი ან ნულის შემოწმება: 
+
+// დაწერეთ კოდი, რომელიც იღებს რიცხვს და ამოწმებს, არის თუ არა ის დადებითი, უარყოფითი ან ნული. შესაბამისად, დაბეჭდეთ "რიცხვი დადებითია", "რიცხვი უარყოფითია" ან "რიცხვი ნულია". 
+// let number = -1
+// if (number > 0) {
+//     console.log('დადებითია')
+// }
+//  else if (number <0) {
+//      console.log('უარყოფითია')
+//  }
+//   else {
+//     console.log('ნულია')
+//  }
+
+
+
+//   სტუდენტის შეფასების განსაზღვრა ქულის მიხედვით: 
+
+// დაწერეთ კოდი, რომელიც იღებს სტუდენტის ქულას (0-დან 100-მდე) და დაბეჭდავს შესაბამის შეფასებას შემდეგი კრიტერიუმების მიხედვით: 
+
+// 90-100: "A" 
+
+// 80-89: "B" 
+
+// 70-79: "C" 
+
+// // 60-69: "D" 
+
+// // 0-59: "F" 
+
+// let score =80;
+// if (score >= 90 && score<=100) {
+// console.log("A");
+// }else if (score  >= 80 && score <= 89){
+//     console.log("B");
+
+//  }else if (score  >= 70 && score <= 79){
+//   console.log("C"); 
+//  } else if (score  >= 60 && score <= 69) {
+//   console.log("D");
+// } else if (score >= 0 && score <= 59 ){
+//     console.log("F");
+//  }
+//    else (score < 0) {
+//     console.log("feil");
+// }
+
+
+//   ასაკის მიხედვით კატეგორიის განსაზღვრა: 
+
+// დაწერეთ კოდი, რომელიც იღებს პირის ასაკს და დაბეჭდავს შესაბამის კატეგორიას: 
+
+// 0-12 წელი: "ბავშვი" 
+
+// 13-17 წელი: "მოზარდი" 
+
+// 18-64 წელი: "ზრდასრული" 
+
+// 65 და მეტი: "ხანდაზმული" 
+
+
+let age =60;
+if (age >= 0 && age<=12) {
+console.log("ბავშვი" );
+}else if (age >=13  && age<=17){
+    console.log("მოზარდი");
+
+}else if (age >=18  && age<=64){
+    console.log("ზრდასრული" );
 }
 
-
-
- tiket.price=30,
- console.log(tiket)
- 
-
+else if (age >65 ){
+    console.log("ხანდაზმული" );
+}
+  
